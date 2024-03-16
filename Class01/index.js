22394e698e5a0f0a8a3d3e81a41828f77d9f466b
usmanashrf <usmanashraf1510@gmail.com>
2024-03-16 20:33:05 +0500

    added first class code

---
+++ Class01/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("This is my first line of code");
+//---------------------------------- Variables ---------------------------------
+let myName = "Usman";
+const myAge = 25;
+console.log(myName, myAge);
---
+++ Class01/index.ts
@@ -0,0 +1,9 @@
+
+console.log("This is my first line of code")
+
+//---------------------------------- Variables ---------------------------------
+
+let myName = "Usman";
+const myAge = 25;
+
+console.log(myName, myAge)
